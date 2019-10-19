96ee856d634a30e6983310964faf0a7d6dee8d43
Ruchita Sahu <ruchisahu1997gmail.com>
2019-10-19 15:48:17 +0530

    initail commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PilgrimagePageComponent } from './pilgrimage-page/pilgrimage-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PilgrimagePageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/pilgrimage-page/pilgrimage-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pilgrimage-page',
+  templateUrl: './pilgrimage-page.component.html',
+  styleUrls: ['./pilgrimage-page.component.css']
+})
+export class PilgrimagePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
